91e0f07fdef1d203351f44cefa10a75d0df51da1
Thomas Hirsch <thokari@posteo.de>
2019-09-17 13:38:05 +0200

    improve import scripts

--- scripts/app-and-client-import.sh
+++ scripts/app-and-client-import.sh
@@ -1,2 +1,4 @@
-curl -X PUT -H "Content-Type: application/json" http://localhost:8088/rs/oauth2/import/clients --data @import-client-payload.json
-curl -X PUT -H "Content-Type: application/json" http://localhost:8088/rs/appstore/import/apps --data @import-app-payload.json
+EPAGESJ_PRIVATE_API=${1:-localhost}
+
+curl -v -X PUT -H "Content-Type: application/json" http://$EPAGESJ_PRIVATE_API:8088/rs/oauth2/import/clients --data @import-client-payload.json
+curl -v -X PUT -H "Content-Type: application/json" http://$EPAGESJ_PRIVATE_API:8088/rs/appstore/import/apps --data @import-app-payload.json
